d0f9b0ee5445ef1e8d5ac9f336b8bd3efe70a94c
Carlos Alberto <81574167+casalfaria@users.noreply.github.com>
2021-07-06 21:04:19 -0300

    Add files via upload

---
+++ Funcoes.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <math.h>
+
+//Funcao de probabilidade Normal
+
+double normal_distribution_pdf(double x, double mu, double std_dev) {
+	double fx;
+	const double pi = 3.14159;
+	
+	
+	fx = (1 / sqrt(2 * pi * std_dev * std_dev)) * exp(-0.5 * (((x - mu) / std_dev) * ((x - mu) / std_dev)));
+
+	return fx;
+
+}
+
+
+
+//Funcao de distribuicao de probabilidade normal
+double normal_distribution_std(double x) {
+
+	double px = normal_distribution_pdf(x, 0, 1);
+
+	return px;
+
+}
